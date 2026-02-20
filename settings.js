003ca73ef5a5af481264a2eef3d24f7b2e17fe5c
D.S.S.KARUNARATHNA <mrdarkhyperlegend@gmail.com>
2026-02-20 20:39:11 +0530

    💗

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botName: "Multi-Device Bot",
+    ownerName: "ඔබේ නම",
+    welcomeMessage: "සාදරයෙන් පිළිගන්නවා! මම Contacts සහ Groups දෙකටම වැඩ කරන වොයිස් බොට්.",
+    welcomeImage: "./media/welcome.jpg", // ඔබේ පින්තූරය media ෆෝල්ඩරයේ තිබිය යුතුය
+    ownerNumber: "947xxxxxxxx@s.whatsapp.net"
+}
