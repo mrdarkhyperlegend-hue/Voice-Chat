820531ceac94cc8b2e25aa9ea9620baeb9db21e8
D.S.S.KARUNARATHNA <mrdarkhyperlegend@gmail.com>
2026-02-20 22:22:54 +0530

    💗

--- mydata.js
+++ mydata.js
@@ -1,5 +1,11 @@
 const voiceMessages = {
-    "hello": "./media/gm.mp3",
+    "hi": "./media/gm.mp3",
+    "ai": "./media/ai.mp3",
+    "bro": "./media/Bro.mp3",
+    "gn": "./media/Gn.mp3",
+    "hmm": "./media/Hmm.mp3",
+    "mk": "./media/gm.mp3",
+    "nama": "./media/Mk.mp3",
     "ගෲප්": "./media/group_info.mp3",
     "සින්දුව": "./media/song.mp3"
 };
