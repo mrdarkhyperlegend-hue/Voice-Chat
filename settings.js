8ba09f37070d98950abe1152ac246625c858f5de
D.S.S.KARUNARATHNA <mrdarkhyperlegend@gmail.com>
2026-02-20 21:32:38 +0530

    💗

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
     botName: "Zukee-MD",
     ownerName: "Suneth Karunarathna",
-    welcomeMessage: "සාදරයෙන් පිළිගන්නවා! මම Contacts සහ Groups දෙකටම වැඩ කරන වොයිස් බොට්.",
+    welcomeMessage: "Hello this is Zukee-MD Voice reply Bot/n Developed By /n D.S.S.Karunarathna",
     welcomeImage: "./media/welcome.jpg", // ඔබේ පින්තූරය media ෆෝල්ඩරයේ තිබිය යුතුය
     ownerNumber: "94770378874@s.whatsapp.net"
 }
